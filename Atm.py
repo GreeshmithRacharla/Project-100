53e488a4aeee09ae4d00d9589bb9242d186c8b08
GreeshmithRacharla <76734059+GreeshmithRacharla@users.noreply.github.com>
2022-02-15 20:49:28 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,19 @@
+
+
+
+from msilib.schema import Class
+
+
+Class Atm(object):
+    def __init__(self,card_number,pin_number):
+        self.card_number = card_number
+        self.pin_number = pin_number
+
+    def CashWithdrawl():
+       print("Cash has been withdrawl successfully!!")
+
+
+    def BalanceEnquiry():
+       print("The balance in your account")
+    
+    
